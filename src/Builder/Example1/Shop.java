19c44d7b869d52747348eae91a29a9410f90db59
venkatesh-jacke <venkateshjacke@gmail.com>
2025-10-15 12:27:24 +0530

    15-10-2025

--- src/Builder/Example1/Shop.java
+++ src/Builder/Example1/Shop.java
@@ -1,5 +1,6 @@
 package Builder.Example1;
 
+//https://www.linkedin.com/posts/sohailashraf31_builder-pattern-activity-7344919287725178880-eYfy?utm_source=share&utm_medium=member_desktop&rcm=ACoAADQfhrABusil0TePI1oqifcQk611TWEQkcs
 public class Shop {
     public static void main(String[] args) {
         PhoneBuilder phoneBuilder = new PhoneBuilder();
--- src/Factory/Example1/Main.java
+++ src/Factory/Example1/Main.java
@@ -1,4 +1,5 @@
 package Factory.Example1;
+//https://www.linkedin.com/posts/sohailashraf31_factory-method-pattern-activity-7344643939745832960-9dWH?utm_source=share&utm_medium=member_desktop&rcm=ACoAADQfhrABusil0TePI1oqifcQk611TWEQkcs
 
 public class Main {
     public static void main(String[] args) {
